f269a7a456da5d9bfaf83ba883b2d8e8c7d7d12b
ezequiel <trabalhojr20@gmail.com>
2020-03-01 11:32:37 -0400

    commit initial

---
+++ src/main/java/com/ezequiel/inventariobackend/InventarioBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ezequiel.inventariobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventarioBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ezequiel/inventariobackend/InventarioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezequiel.inventariobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
